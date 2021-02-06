93ef6f2d79fceaa60ee3cd9d74f9bad5bce0134b
Geoffrey Poore <gpoore@gmail.com>
2021-02-05 21:04:47 -0600

    updated config and customization for current mkdocs version

--- change_highlight_color.py
+++ change_highlight_color.py
@@ -1,14 +1,8 @@
-string_color = '#009926'
+code_color = '#009926'
 
-with open('site/css/highlight.css', encoding='utf8') as f:
+with open('site/css/theme_extra.css', encoding='utf8') as f:
     data = f.read()
-data = data.replace('#d14', string_color)
-with open('site/css/highlight.css', 'w', encoding='utf8', newline='\n') as f:
-    f.write(data)
-
-with open('site/css/theme.css', encoding='utf8') as f:
-    data = f.read()
-before, after = data.split('.rst-content tt{white-space:nowrap;')
-data = before + '.rst-content tt{white-space:nowrap;' + after.replace('#E74C3C', string_color, 1)
-with open('site/css/theme.css', 'w', encoding='utf8', newline='\n') as f:
+before, after = data.rsplit('.rst-content code {', maxsplit=1)
+data = before + '.rst-content code {' + after.replace('#E74C3C', code_color, 1)
+with open('site/css/theme_extra.css', 'w', encoding='utf8', newline='\n') as f:
     f.write(data)
